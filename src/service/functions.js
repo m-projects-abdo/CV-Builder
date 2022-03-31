75f36ac2e5547f1e2539342794afb436e034f667
diala <diala.alassaf@tatweer.sy>
2022-03-31 15:21:53 +0300

    add openPopup and skill form

--- src/service/functions.js
+++ src/service/functions.js
@@ -1,5 +1,38 @@
 let popup = document.getElementById('add-skill-popup');
+let skillContainer = document.getElementById('skill-container');
+let skillNameInput = document.getElementById('skill-name');
+let skillName = '';
+
+let skillTemplate = (skillName) => `
+  <div class="col-md-4 px-4 d-flex align-items-center justify-content-between mb-4">
+    <p class="font-weight-bold h4">${skillName}</p>
+    <div class="d-flex justify-content-between position-relative">
+      <span class="circle-component border border-warning"></span>
+      <span class="circle-component border border-warning mx-1"></span>
+      <span class="circle-component border border-warning"></span>
+      <span class="circle-component border border-warning mx-1"></span>
+      <span class="circle-component border border-warning"></span>
+      <box-icon
+        onclick="deleteSkill(this)" 
+        class="message-x position-absolute"
+        name='message-x' 
+        type='solid' 
+        color='rgba(249,22,22,0.88)'
+      ></box-icon>
+    </div>
+  </div>
+`
 
 //Delete function
 const deleteSkill = (event) => event.parentNode.parentNode.remove();
-const closePopup = _ => popup.style.display = none;
+const openPopup = _ => popup.style.display = 'block';
+const closePopup = _ => popup.style.display = 'none';
+const addSkill = _ => {
+  skillContainer.innerHTML += skillTemplate(skillName);
+  skillNameInput.value = '';
+  closePopup();
+}
+
+skillNameInput.addEventListener('input', (event)=> {
+  skillName = event.target.value;
+})
